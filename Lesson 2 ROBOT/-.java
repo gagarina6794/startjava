3e1f12f6729d3caf266367b9788b23ae2a130c0b
gagarina6794 <gagarina6794@gmail.com>
2018-05-16 17:25:23 +0300

    java file

---
+++ Lesson 2 ROBOT/-.java
@@ -0,0 +1,28 @@
+
+public class Jaeger {
+
+	String modelName = "Bracer Phoenix";
+	String mark = "Mark-5";
+	String origin = "USA";
+	float height = 70.7;
+	float weight = 2.1;
+	int speed = 3;
+	int strength = 8;
+	int armor = 9;
+
+	boolean drift() {
+
+	}
+
+	void move() {
+
+	}
+
+	String scanKaiju() {
+
+	} 
+
+	void useVortexCannon() {
+
+	}
+}
